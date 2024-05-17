befec76dfed03d9a3f11de4fa4c27970b906dc09
Jesse Nicholas <jessenicholas.t@gmail.com>
2024-05-18 03:08:18 +0700

    add missing validation for update nip

--- controllers/auth/update_nurse.go
+++ controllers/auth/update_nurse.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/JesseNicholas00/HaloSuster/services/auth"
+	"github.com/JesseNicholas00/HaloSuster/types/nip"
 	"github.com/JesseNicholas00/HaloSuster/utils/errorutil"
 	"github.com/JesseNicholas00/HaloSuster/utils/request"
 	"github.com/labstack/echo/v4"
@@ -14,6 +15,12 @@ func (ctrl *authController) updateNurse(c echo.Context) error {
 	var req auth.UpdateNurseReq
 	if err := request.BindAndValidate(c, &req); err != nil {
 		return err
+	}
+
+	if nip.GetRole(req.Nip) != nip.RoleNurse {
+		return echo.NewHTTPError(http.StatusNotFound, echo.Map{
+			"message": "not a valid nip",
+		})
 	}
 
 	var res auth.UpdateNurseRes
